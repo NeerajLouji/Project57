f57c0ce08f1f9bb02dd603e37cbf1b98b7341685
NeerajLouji <loujin@sheridancollege.ca>
2019-05-15 15:35:55 -0400

    Demo project57

--- src/arrayofobject/ArrayOfObject.java
+++ src/arrayofobject/ArrayOfObject.java
@@ -11,7 +11,13 @@ package arrayofobject;
  */
 public class ArrayOfObject {
     private String name;
-
+    private int sid;
+    
+public ArrayOfObject(String s,int n)
+{
+    name=s;
+    sid=n;
+}
  
     public String getName() {
         return name;
@@ -19,6 +25,20 @@ public class ArrayOfObject {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
     }
     
     
